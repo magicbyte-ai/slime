962947f75ecb5f3414bc5d617f82021b614ed2ba
Yenting Lin <ytl@ieee.org>
2025-08-01 11:11:14 +0000

    feat(scripts): add convert-qwen3-4B.sh for model conversion and update run-qwen3-4B.sh for improved path handling
    - Introduced a new script `convert-qwen3-4B.sh` to facilitate the conversion of the Qwen3-4B model using Ray.
    - Updated `run-qwen3-4B.sh` to streamline model paths and environment variables, enhancing clarity and maintainability.

---
+++ scripts/convert-qwen3-4B.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+set -ex
+
+source scripts/models/qwen3-4B.sh
+
+MODEL_HOME="/data/post_train/models/"
+MEGATRON_CKPT_PATH="${MODEL_HOME}/Qwen3-4B_slime_yenting"
+
+RAY_HEAD_ADDRESS="http://147.185.41.214:30265"
+
+# RAY_ENABLE_RECORD_ACTOR_TASK_LOGGING=1
+RUNTIME_ENV_JSON="{
+  \"env_vars\": {
+    \"PYTHONPATH\": \"/data/post_train/Megatron-LM/\",
+    \"CUDA_DEVICE_MAX_CONNECTIONS\": \"1\",
+    \"NCCL_NVLS_ENABLE\": \"${HAS_NVLINK}\",
+    \"RAY_ENABLE_RECORD_ACTOR_TASK_LOGGING\": \"1\"
+  }
+}"
+
+ray job submit --address="${RAY_HEAD_ADDRESS}" \
+   --working-dir="." \
+   --runtime-env-json="${RUNTIME_ENV_JSON}" \
+   -- python3 tools/convert_hf_to_torch_dist.py \
+    ${MODEL_ARGS[@]} \
+    --hf-checkpoint "${MODEL_HOME}/Qwen3-4B" \
+    --save "${MEGATRON_CKPT_PATH}"
